86b61124f2957b420c222a5c2d6eb12626794809
LukeMcK <19780449+LukeMcK@users.noreply.github.com>
2023-09-29 14:00:17 -0400

    added eslint to tidy up files

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Person = require("./models/person");
 const morgan = require("morgan");
 const cors = require("cors");
 
-morgan.token("body", (req, res) => JSON.stringify(req.body));
+morgan.token("body", (req) => JSON.stringify(req.body));
 app.use(
   morgan(
     ":method :url :status :response-time ms - :res[content-length] :body - :req[content-length]"
@@ -32,39 +32,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-  {
-    id: 5,
-    name: "Maya",
-    number: "978-210-5908",
-  },
-];
-
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/info", (request, response) => {
+app.get("/info", (request, response, next) => {
   Person.countDocuments()
     .then((result) => {
       const message = `<p>Phonebook has info for ${result} people<br />${new Date()}</p>`;
@@ -87,15 +59,11 @@ app.get("/api/persons/:id", (request, response) => {
 
 app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-    .then((result) => {
+    .then(() => {
       response.status(204).end();
     })
     .catch((error) => next(error));
 });
-
-const generateId = () => {
-  return Math.floor(Math.random() * 100000);
-};
 
 app.post("/api/persons", (request, response, next) => {
   const body = request.body;
